440eab679d2176ae4b6c099ede509085e2003823
Patrick <150119622+PatrickAspIT@users.noreply.github.com>
2023-12-19 10:15:24 +0100

    Add files via upload

---
+++ CodeFolder/WPF/Projekter med Jens/WpfIntroduktion2/BIZ/ClassBIZ.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace BIZ
+{
+    public class ClassBIZ
+    {
+        Random rnd = new Random();
+
+        public ClassBIZ()
+        {
+                
+        }
+
+        public void Opgave1(ListBox inListBox)
+        {
+            for (int i = 4711; i < 4737; i++)
+            {
+                inListBox.Items.Add(i.ToString());
+            }
+        }
+
+        public void Opgave2(ListBox inListBox)
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                inListBox.Items.Add(rnd.Next(100000, 1000001));
+            }
+        }
+
+        public void Opgave3(ListBox inListBox)
+        {
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                newList.Add(rnd.Next(100000, 1000001));
+            }
+
+            newList.Sort();
+
+            foreach (int i in newList)
+            {
+                inListBox.Items.Add(i.ToString());
+            }
+        }
+
+        public void Opgave4(ListBox inListBox)
+        {         
+            List<int> sortedList = new List<int>();        
+
+            for (int i = 0; i < 25; i++)
+            {
+                sortedList.Add(rnd.Next(100000, 1000001));
+            }
+
+            List<int> unsortedList = new List<int>(sortedList);
+            sortedList.Sort();
+
+            for (int i = 0; i < 25; i++)
+            {
+                inListBox.Items.Add($"{unsortedList[i]} - {sortedList[i]}");
+            }
+        }
+
+        public void Opgave5(ListBox inListBox)
+        {
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomNumber = rnd.Next(100000, 1000001);
+                list.Add(randomNumber);
+                inListBox.Items.Add(randomNumber);
+            }
+
+            inListBox.Items.Add("");
+            inListBox.Items.Add($"Gennemsnitsværdi: {GetAverageValue(list)}");
+        }
+
+        private int GetAverageValue(List<int> list)
+        {
+            int intRes = 0;
+            int intCount = 0;
+
+            foreach (int tal in list) 
+            { 
+                intRes += tal;
+                intCount++;
+            }
+
+            intRes = intRes / intCount;
+
+            return intRes;
+        }
+
+        public void Opgave6(ListBox inListBox)
+        {
+            List<int> newList = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                newList.Add(rnd.Next(100000, 1000001));
+            }
+
+            newList.Sort();
+            int intAverage = GetAverageValue(newList);
+
+            foreach (int number in newList)
+            {
+                inListBox.Items.Add($"{number} - {intAverage} = {number - intAverage}");
+            }
+        }
+
+        public List<string> Opgave7()
+        {
+            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomNumber = rnd.Next(100000, 1000001);
+                intList.Add(randomNumber);
+            }
+
+            int intAverage = GetAverageValue(intList);
+            intList.Sort();
+
+            foreach (int tal in intList)
+            {
+                stringList.Add($"{tal} - {intAverage} = {tal - intAverage}");
+            }
+
+            return stringList;
+        }
+
+        public void Opgave8(ListBox inListBox)
+        {
+            List<int> intList = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomNumber = rnd.Next(100000, 1000001);
+                intList.Add(randomNumber);
+            }
+
+            int intAverage = GetAverageValue(intList);
+            intList.Sort();
+
+            foreach (int tal in intList)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if ((tal % 2) != 0)
+                {
+                    listBoxItem.Background = Brushes.AliceBlue;
+                }
+                else
+                {
+                    listBoxItem.Background = Brushes.HotPink;
+                }
+
+                listBoxItem.Content = $"{tal} - {intAverage} = {tal - intAverage}";
+                inListBox.Items.Add(listBoxItem);
+            }
+        }
+
+        public void Opgave9(ListBox inListBox)
+        {
+            List<int> intList = new List<int>();
+            List<ListBoxItem> list = new List<ListBoxItem>();
+
+            for (int i = 0; i < 25; i++)
+            {
+                int randomNumber = rnd.Next(100000, 1000001);
+                intList.Add(randomNumber);
+            }
+
+            int intAverage = GetAverageValue(intList);
+            intList.Sort();
+
+            foreach (int tal in intList)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if ((tal % 2) != 0)
+                {
+                    listBoxItem.Background = Brushes.AliceBlue;
+                }
+                else
+                {
+                    listBoxItem.Background = Brushes.HotPink;
+                }
+
+                listBoxItem.Content = $"{tal} - {intAverage} = {tal - intAverage}";
+                list.Add(listBoxItem);
+                
+            }
+            inListBox.ItemsSource = list;
+        }
+    }
+}
---
+++ CodeFolder/WPF/Projekter med Jens/WpfIntroduktion2/GUI/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using BIZ;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ClassBIZ BIZ;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            BIZ = new ClassBIZ();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button myB = (Button)sender;
+            Menu(int.Parse(myB.Tag.ToString()));
+        }
+
+        private void Menu(int inKey)
+        {
+            ClearListBox();
+
+            switch (inKey)
+            {
+                case 1:
+                    BIZ.Opgave1(ListBoxRes);
+                    break;
+                case 2:
+                    BIZ.Opgave2(ListBoxRes);
+                    break;
+                case 3:
+                    BIZ.Opgave3(ListBoxRes);
+                    break;
+                case 4:
+                    BIZ.Opgave4(ListBoxRes);
+                    break;
+                case 5:
+                    BIZ.Opgave5(ListBoxRes);
+                    break;
+                case 6:
+                    BIZ.Opgave6(ListBoxRes);
+                    break;
+                case 7:
+                    ListBoxRes.ItemsSource = BIZ.Opgave7();
+                    break;
+                case 8:
+                    BIZ.Opgave8(ListBoxRes);
+                    break;
+                case 9:
+                    BIZ.Opgave9(ListBoxRes);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ClearListBox()
+        {
+            if (ListBoxRes.ItemsSource != null)
+            {
+                ListBoxRes.ItemsSource = null;
+            }
+            ListBoxRes.Items.Clear();
+        }
+    }
+}
