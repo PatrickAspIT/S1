42416e9a471dc4a48ab499f523dda2a2d3677132
Patrick <150119622+PatrickAspIT@users.noreply.github.com>
2023-12-19 10:11:55 +0100

    Add files via upload

---
+++ CodeFolder/WPF/Flydekasse 2022 .Net Framework/Repository/ClassMaterial.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    internal class ClassMaterial : ClassNotify
+    {
+        private double _treeWeight;
+        private double _plasticWeight;
+        private double _glassWeight;
+        private double _ironWeight;
+
+        public ClassMaterial()
+        {
+            treeWeight = 0.0;
+            plasticWeight = 0.0;
+            glassWeight = 0.0;
+            ironWeight = 0.0;
+        }
+
+        // Vægt i kg for træ, plast, glas og jern
+        public double treeWeight
+        {
+            get { return _treeWeight; }
+            private set
+            {
+                if (_treeWeight != value)
+                {
+                    _treeWeight = value;
+                }                    
+                Notify("treeWeight");
+            }
+        }
+        public double plasticWeight
+        {
+            get { return _plasticWeight; }
+            private set
+            {
+                if (_plasticWeight != value)
+                {
+                    _plasticWeight = value;
+                }                    
+                Notify("plasticWeight");
+            }
+        }
+        public double glassWeight
+        {
+            get { return _glassWeight; }
+            private set
+            {
+                if (_glassWeight != value)
+                {
+                    _glassWeight = value;
+                }                    
+                Notify("glassWeight");
+            }
+        }
+        public double ironWeight
+        {
+            get { return _ironWeight; }
+            private set
+            {
+                if (_ironWeight != value)
+                {
+                    _ironWeight = value;
+                }                    
+                Notify("ironWeight");
+            }
+        }
+
+
+    }
+}
