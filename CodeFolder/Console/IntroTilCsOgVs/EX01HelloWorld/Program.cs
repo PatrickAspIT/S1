9fd3496fe27004cf866a9f4efa35e0ed148cee2a
Patrick <150119622+PatrickAspIT@users.noreply.github.com>
2023-12-19 09:08:09 +0100

    Add files via upload

---
+++ CodeFolder/Console/IntroTilCsOgVs/EX01HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX01HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string schoolName = "AspIT";
+
+            Console.WriteLine(schoolName);
+            Console.ReadLine();
+
+        }
+    }
+}
