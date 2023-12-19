0019e881f31b6da552d0f5aafb41038e66dc1099
Patrick <150119622+PatrickAspIT@users.noreply.github.com>
2023-12-19 09:32:07 +0100

    Add files via upload

---
+++ CodeFolder/WPF/WpfAppIntroduktion/BIZ/ClassBIZ.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfAppIntroduktion.BIZ
+{
+    internal class ClassBIZ
+    {
+        // Opgave 1.2
+        public void Number4711(ListBox listBox) 
+        {
+            ClearList(listBox);
+
+            for (int i = 4711; i <= 4736; i++)
+                listBox.Items.Add(i.ToString());
+        }
+
+        // Opgave 1.3
+        public void RandomNumber(ListBox listBox) 
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            int randomNumber = 0;
+            
+            for (int i = 0; i < 25; i++)
+            {
+                randomNumber = rnd.Next(100000, 1000001);
+                listBox.Items.Add(randomNumber.ToString());
+            }
+        }
+
+        // Opgave 1.4
+        public void RandomSort(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+
+            for (int i = 0;i < 25; i++)
+                list.Add(rnd.Next(100000, 1000001));
+
+            list.Sort();
+
+            foreach (int i in list)
+                listBox.Items.Add(i.ToString());
+        }
+
+        // Opgave 1.5
+        public void TwoLists(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> intListUnsorted = new List<int>();
+            List<int> intListSorted = new List<int>();
+            int randomNumber = 0;
+
+            for(int i = 0; i < 25; i++)
+            {
+                randomNumber = rnd.Next(100000, 1000001);
+                intListSorted.Add(randomNumber);
+                intListUnsorted.Add(randomNumber);
+            }
+
+            intListSorted.Sort();
+
+            for(int i = 0; i < intListSorted.Count; i++)
+            {
+                listBox.Items.Add($"{intListUnsorted[i]} - {intListSorted[i]}");
+            }
+        }
+
+        // Opgave 1.6
+        public void AverageValue(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> newList = new List<int>();
+            int randomNumber = 0;
+
+            for (int i = 0; i < 25; i++)
+            {
+                randomNumber = rnd.Next(100000, 1000001);
+                listBox.Items.Add(randomNumber.ToString());
+                newList.Add(randomNumber);
+            }
+
+            listBox.Items.Add("");
+            listBox.Items.Add($"Gennemsnitsværdi: {CalcAverageValue(newList)}");
+        }
+
+        private int CalcAverageValue(List<int> list)
+        {
+            int totalNumber = 0;
+            int ammountInList = 0;
+
+            foreach (int i in list)
+            {
+                totalNumber += i;
+                ammountInList++;
+            }
+            totalNumber = totalNumber / ammountInList;
+            return totalNumber;
+        }
+
+        // Opgave 1.7
+        public void ThreeValues(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> newList = new List<int>();
+
+            for(int i = 0;i < 25; i++)
+            {
+                newList.Add(rnd.Next(100000, 1000001));
+            }
+
+            int AverageNumber = CalcAverageValue(newList);
+
+            foreach (int i in newList)
+            {
+                listBox.Items.Add($"{i} - {AverageNumber} = {i - AverageNumber}");
+            }
+        }
+
+        // Opgave 1.8
+        public List<string> NoListBox()
+        {
+            Random rnd = new Random();
+            List<int> intList = new List<int>();
+            List<string> stringList = new List<string>();
+
+            for (int i = 0; i < 25; i++)
+                intList.Add(rnd.Next(100000, 1000001));
+
+            int averageValue = CalcAverageValue(intList);
+            intList.Sort();
+
+            foreach (int i in intList)
+                stringList.Add($"{i} + {averageValue} = {i - averageValue}");
+
+            return stringList;
+        }
+
+        private void ClearList(ListBox listBox)
+        {
+            if (listBox.ItemsSource != null)
+                listBox.ItemsSource = null;
+            listBox.Items.Clear();
+        }
+
+        // Opgavee 1.9
+        public void BackgroundColor(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> intList = new List<int>();
+
+            for (int i = 0; i < 25; i++)
+                intList.Add(rnd.Next(100000, 1000001));
+
+            int averageNumber = CalcAverageValue(intList);
+            intList.Sort();
+
+            foreach(int i in intList)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if ((i % 2) != 0)
+                    listBoxItem.Background = Brushes.AliceBlue;
+                else
+                    listBoxItem.Background= Brushes.HotPink;
+
+                listBoxItem.Content = $"{i} + {averageNumber} = {i - averageNumber}";
+                listBox.Items.Add(listBoxItem);
+            }
+        }
+
+        // Opgave 1.10
+        public List<ListBoxItem> FinalAssignment(ListBox listBox)
+        {
+            ClearList(listBox);
+
+            Random rnd = new Random();
+            List<int> intList = new List<int>();
+            List<ListBoxItem> list = new List<ListBoxItem>();
+
+            for (int i = 0; i < 25; i++)
+                intList.Add(rnd.Next(100000, 1000001));
+
+            int averageValue = CalcAverageValue(intList);
+            intList.Sort();
+
+            foreach (int i in intList)
+            {
+                ListBoxItem listBoxItem = new ListBoxItem();
+                if ((i % 2) != 0)
+                    listBoxItem.Background = Brushes.AliceBlue;
+                else
+                    listBoxItem.Background = Brushes.HotPink;
+
+                listBoxItem.Content = $"{i} + {averageValue} = {i - averageValue}";
+                list.Add(listBoxItem);
+            }
+
+            return list;
+        }
+
+    }
+}
---
+++ CodeFolder/WPF/WpfAppIntroduktion/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppIntroduktion.BIZ;
+
+namespace WpfAppIntroduktion
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        // Opgave 1.1
+        ClassBIZ classBIZ = new ClassBIZ();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Opgave 1.2
+        private void buttonOpg12_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.Number4711(listBoxRes);
+        }
+
+        // Opgave 1.3
+        private void buttonOpg13_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.RandomNumber(listBoxRes);
+        }
+
+        // Opgave 1.4
+        private void buttonOpg14_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.RandomSort(listBoxRes);
+        }
+
+        // Opgave 1.5
+        private void buttonOpg15_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.TwoLists(listBoxRes);
+        }
+
+        // Opgave 1.6
+        private void buttonOpg16_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.AverageValue(listBoxRes);
+        }
+
+        // Opgave 1.7
+        private void buttonOpg17_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.ThreeValues(listBoxRes);
+        }
+
+        // Opgave 1.8
+        private void buttonOpg18_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.ItemsSource = null;
+            listBoxRes.Items.Clear();
+            listBoxRes.ItemsSource = classBIZ.NoListBox();
+        }
+
+        // Opgave 1.9
+        private void buttonOpg19_Click(object sender, RoutedEventArgs e)
+        {
+            classBIZ.BackgroundColor(listBoxRes);
+        }
+
+        // Opgave 1.10
+        private void buttonOpg20_Click(object sender, RoutedEventArgs e)
+        {
+            listBoxRes.ItemsSource = classBIZ.FinalAssignment(listBoxRes);
+        }
+    }
+}
